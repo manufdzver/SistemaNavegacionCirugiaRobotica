0ddf5d6159ee1cae143afdfacc1edae6cf16e11a
Manuel Fernandez <manufdzver@hotmail.com>
2022-11-06 22:47:42 -0600

    controlador ibvs

--- main.py
+++ main.py
@@ -6,13 +6,39 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 import scipy.stats as stats
 
+#Yellow
+b_yellow=39
+g_yellow=224
+r_yellow=230
+
+#Green
+b_green=63
+g_green=123
+r_green=75
+
+#Blue
+b_blue=210
+g_blue=150
+r_blue=98
+
+def paintWhite(path, height, width):
+    path='img/imgActual.png'
+    frame=cv2.imread(path)
+    frame[height,width,0]=255
+    frame[height,width,1]=255
+    frame[height,width,2]=255
+    cv2.imwrite(path,frame)
+
+def paintBlack(path, height, width):
+    frame=cv2.imread(path)
+    frame[height,width,0]=0
+    frame[height,width,1]=0
+    frame[height,width,2]=0
+    cv2.imwrite(path,frame)
+
 def cleanValues(coordinatesArray):
     x=coordinatesArray[:,0]
     y=coordinatesArray[:,1]
-    plt.xlim(0,480)
-    plt.ylim(0,640)
-    plt.scatter(x,y,color = "g", marker = ".") 
-    plt.show()
     
     mean_x = np.mean(x, axis=0)
     sd_x = np.std(x, axis=0)
@@ -24,85 +50,89 @@ def cleanValues(coordinatesArray):
     final_list = [a for a in coordinatesArray if ((a[0] > (mean_x - des * sd_x)) and (a[1] > (mean_y - des * sd_y)))]
     final_list = [a for a in final_list if ((a[0] < (mean_x + des * sd_x)) and (a[1] < (mean_y + des * sd_y)))]
     final_list = np.array(final_list)
-    
-    x=final_list[:,0]
-    y=final_list[:,1]
-    plt.xlim(0,480)
-    plt.ylim(0,640)
-    plt.scatter(x,y,color = "g", marker = ".") 
-    plt.show()
 
     return final_list
-
 
 def centroid(coordinatesArray):
     n = len(coordinatesArray)  
     return (np.sum(coordinatesArray,axis=0)/n).astype(int)
 
-
-def diferencia():
-    frame=cv2.imread('img/imgActual.png')
-    height, width, channels = frame.shape
-    #Yellow
-    blue=39
-    green=224
-    red=230
-    #Green
-    #blue=63
-    #green=123
-    #red=75
-    #Blue
-    #blue=210
-    #green=150
-    #red=98
+def getMarkersGroup(image, blue, green, red):
     overhead=30
-    # 594 124 83
-    pixelsPerPoint=0
+    height, width, channels = image.shape
     coordinates = np.empty((0, 2), int)
     for x in range(0, height-1) :
      for y in range(0, width-1) :
         
-        b=frame[x,y,0] #B Channel Value
-        g=frame[x,y,1] #G Channel Value
-        r=frame[x,y,2] #R Channel Value
+        b=image[x,y,0] #B Channel Value
+        g=image[x,y,1] #G Channel Value
+        r=image[x,y,2] #R Channel Value
         #Yellow
         if (blue-overhead)<=b<=(blue+overhead) and (green-overhead)<=g<=(green+overhead) and (red-overhead)<=r<=(red+overhead):
-            paintWhite(x,y)
+            paintWhite('img/imgActual.png',x,y)
             coordinates = np.append(coordinates,np.array([[x,y]]), axis=0)
-            pixelsPerPoint = pixelsPerPoint +1
-        
-    print("Pixels:"+str(pixelsPerPoint))
-    coordinates = cleanValues(coordinates)
-    center=centroid(coordinates)
-    #print(coordinates)
-    print(center)
-    paintBlack(center[0], center[1])
+    return coordinates
 
-    
+def locateMarkers(image):
+    blueCoordinates=getMarkersGroup(image, b_blue, g_blue, r_blue)
+    greenCoordinates=getMarkersGroup(image, b_green, g_green, r_green)
+    yellowCoordinates=getMarkersGroup(image, b_yellow, g_yellow, r_yellow)
 
-def paintWhite(height, width):
-    frame=cv2.imread('img/imgActual.png')
-    frame[height,width,0]=255
-    frame[height,width,1]=255
-    frame[height,width,2]=255
-    cv2.imwrite('img/imgActual.png',frame)
+    blueCoordinates=cleanValues(blueCoordinates)
+    greenCoordinates=cleanValues(greenCoordinates)
+    yellowCoordinates=cleanValues(yellowCoordinates)
 
-def paintBlack(height, width):
-    frame=cv2.imread('img/imgActual.png')
-    frame[height,width,0]=0
-    frame[height,width,1]=0
-    frame[height,width,2]=0
-    cv2.imwrite('img/imgActual.png',frame)
+    centerBlue = centroid(blueCoordinates)
+    centerGreen = centroid(greenCoordinates)
+    centerYellow = centroid(yellowCoordinates)
+
+    coordinates = np.empty((0, 2), int)
+    coordinates = np.append(coordinates,np.array([centerBlue]), axis=0)
+    coordinates = np.append(coordinates,np.array([centerGreen]), axis=0)
+    coordinates = np.append(coordinates,np.array([centerYellow]), axis=0)
+
+    return coordinates
+
+def diferencia():
+    imgActual=cv2.imread('img/imgActual.png')
+    imgDeseada=cv2.imread('img/imgDeseada.png')
+
+    #Coordinates blue green yellow
+    coordinatesImgActual = locateMarkers(imgActual)
+
+    #Coordinates blue green yellow
+    coordinatesImgDeseada = locateMarkers(imgDeseada)
+
+    error = np.subtract(coordinatesImgDeseada, coordinatesImgActual)
+    return error
+
+def testing(image):
+    blueCoordinates=getMarkersGroup(image, b_blue, g_blue, r_blue)
+    greenCoordinates=getMarkersGroup(image,  b_green, g_green, r_green)
+    yellowCoordinates=getMarkersGroup(image, b_yellow, g_yellow, r_yellow)
+
+    blueCoordinates=cleanValues(blueCoordinates)
+    greenCoordinates=cleanValues(greenCoordinates)
+    yellowCoordinates=cleanValues(yellowCoordinates)
+
+    centerBlue = centroid(blueCoordinates)
+    centerGreen = centroid(greenCoordinates)
+    centerYellow = centroid(yellowCoordinates)
+
+    coordinates = np.empty((0, 2), int)
+    coordinates = np.append(coordinates,np.array([centerBlue]), axis=0)
+    coordinates = np.append(coordinates,np.array([centerGreen]), axis=0)
+    coordinates = np.append(coordinates,np.array([centerYellow]), axis=0)
+
+    return coordinates
 
 def main():
     #lecturaImagenActual.main()
     #controladorIBVS.main()
-    diferencia()
-    #print("Done!")
-
-    #array = np.array([[1,2],[3,4],[5,6]])
-    #print(array[:,0])
-    #plt.scatter(x, y, color = "g", marker = "o") 
+    error = diferencia()
+    print("Error:")
+    print(error)
+    print("Done!")
 
 if __name__ == "__main__":
     main()
